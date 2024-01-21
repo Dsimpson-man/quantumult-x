e6f5027b281d61a6caa7f2cbfd8059366d28af90
Dsimpson-man <88556801+Dsimpson-man@users.noreply.github.com>
2024-01-22 05:40:41 +0800

    更新 zhongtitong.js

--- zhongtitong.js
+++ zhongtitong.js
@@ -6,6 +6,6 @@ obj.loginname = "o-5mbv-PKzMhG_xi6m1Bhw8MDgGI";
 obj.diff = "2222222";
 obj.freegold = "2222222";
 obj.memberdays = "2222";
-obj.package = "6";
+obj.package = "15";
 
 $done({ body: JSON.stringify(obj) });
